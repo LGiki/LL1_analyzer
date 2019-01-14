726e1690abfa1402652c94650da6b808a224aef9
LGiki <qq547221432@gmail.com>
2019-01-14 16:43:29 +0800

    Fix comments error

--- main.cpp
+++ main.cpp
@@ -26,8 +26,8 @@ void getFIRST() {   //计算 FIRST 集合
             int firstSize = FIRST[nonTerminalChar].size();
             for (string rightSide : GRAMMAR[nonTerminalChar]) {  //遍历产生式右部
                 for (char ch : rightSide) {     //遍历产生式右部所有字符
-                    if (!isupper(ch)) {     //如果是非终结符
-                        FIRST[nonTerminalChar].insert(ch);  //将该非终结符加入到 nonTerminalChar 的 First 集合中
+                    if (!isupper(ch)) {     //如果是终结符
+                        FIRST[nonTerminalChar].insert(ch);  //将该终结符加入到 nonTerminalChar 的 First 集合中
                         break;      //退出循环
                     } else {
                         bool flag = false;      //判断 First 集合是否存在空串
@@ -75,19 +75,19 @@ void getFOLLOW() {  //计算 FOLLOW 集合
                             }
                         }
                         for (; i < rightSide.length(); i++) {   //遍历后续字符
-                            if (!isupper(rightSide[i])) {   //如果是非终结符
+                            if (!isupper(rightSide[i])) {   //如果是终结符
                                 FOLLOW[nonTerminalChar].insert(
-                                        rightSide[i]);   //直接将非终结符加入到 nonTerminalChar 的 Follow 集合中
+                                        rightSide[i]);   //直接将终结符加入到 nonTerminalChar 的 Follow 集合中
                                 break;      //直接退出
-                            } else {        //是终结符
-                                for (char ch : FIRST[rightSide[i]]) {   //将该终结符的 First 集合加入到 nonTerminalChar 的 Follow 集合中（除了空串）
+                            } else {        //是非终结符
+                                for (char ch : FIRST[rightSide[i]]) {   //将该非终结符的 First 集合加入到 nonTerminalChar 的 Follow 集合中（除了空串）
                                     if (ch != '@') {
                                         FOLLOW[nonTerminalChar].insert(ch);
                                     }
                                 }
-                                if (!toEpsilon[rightSide[i]]) {     //如果该终结符的 First 集合中不存在空串，则退出循环
+                                if (!toEpsilon[rightSide[i]]) {     //如果该非终结符的 First 集合中不存在空串，则退出循环
                                     break;
-                                } else if (i == rightSide.length() - 1) {        //该终结符的 First 集合中存在空串，且为右部最后一个字符
+                                } else if (i == rightSide.length() - 1) {        //该非终结符的 First 集合中存在空串，且为右部最后一个字符
                                     for (char ch : FOLLOW[iter->first]) {
                                         FOLLOW[nonTerminalChar].insert(
                                                 ch);     //将左部非终结符的 Follow 加入到 nonTerminalChar 的 Follow 集合中
@@ -368,7 +368,7 @@ void analysis() {
     TablePrinter tp(&std::cout);
     tp.AddColumn("Stack", 10);
     tp.AddColumn("Input", 20);
-    tp.AddColumn("Action", 35);
+    tp.AddColumn("Action", 40);
     tp.PrintHeader();
     vector<string> analysisStack;
     //栈初始化
